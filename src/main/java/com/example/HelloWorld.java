6eb38d9f364e955b195d734494bf53f94caba4bf
Alexander Kel <1906.kas@gmail.com>
2016-04-25 00:18:22 +0300

    New for heroku

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,11 @@ public class HelloWorld {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String helloWorld() {
         return "Hello, World!";
+    }
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String helloName(@PathVariable String name) {
+        return "Hello, " + name;
     }
 
 }

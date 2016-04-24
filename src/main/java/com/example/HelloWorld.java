57bb1052036d3980eaa6d88912df33773cedf370
Alexander Kel <1906.kas@gmail.com>
2016-04-24 23:43:00 +0300

    Initial

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Alex on 24.04.16.
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+
+}

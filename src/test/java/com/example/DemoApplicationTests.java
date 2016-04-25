ea3a045cf8e3c8fe9a622fdde5e88f1fc78def72
Alexander Kel <1906.kas@gmail.com>
2016-04-25 17:40:18 +0300

    Test fails

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import static org.junit.Assert.fail;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 @WebAppConfiguration
@@ -14,6 +16,11 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 
+	}
+
+	@Test
+	public void shoulFail() {
+		fail();
 	}
 
 }
